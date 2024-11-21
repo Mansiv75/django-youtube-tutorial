61416cea152691d92b74cc6193333b087dfab37e
Mansiv75 <mv072004@gmail.com>
2024-11-21 19:06:49 +0530

    updated files

--- core/home/views.py
+++ core/home/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 # whole logic part is return in the views file.
 
 def home(request):
-    return HttpResponse("Hey I am a Django Server.")
+    return render(request, 'home\index.html')
+
+def success_page(request):
+    return HttpResponse("<h1>This is a success page.</h1>")
