2f1c701f1bb44b10c6ea1073091866806b80df0d
Mansiv75 <mv072004@gmail.com>
2024-11-23 23:50:45 +0530

    migrating to the databse

--- core/home/models.py
+++ core/home/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+#schema defines the structure of the databse
+#the entire schema is defined in the models.py file
+
+class Student(models.Model):
+    #id=models.AutoField(primary_key=True)
+    name= models.CharField(max_length=100)
+    age =models.IntegerField()
+    email=models.EmailField()
+    address=models.TextField(null=True, blank=True)
+    image=models.ImageField(null=True, blank=True)
+    file=models.FileField(null=True, blank=True)
+
+
+class Product(models.Model):
+    pass    
+
+
+#everytime a change is made to the model file, 
+#python manage.py makemigrations
+#python manage.py migrate
+
+#if i delete the 0001 file, database will crash
+# #if i delete any of the migration files, django will know, bcz it creates an entry for each migration in its database. and hhence the app will crash
+
+# everytime i hit the migrate command , it creates a state for the model and compares with the older state and thats how it knows, there are changes made to the database
