406b052800ba0cb1117cbb001f46d176b3a2191b
Mansiv75 <mv072004@gmail.com>
2024-11-29 15:00:59 +0530

    connected backend with frontend

--- core/vege/views.py
+++ core/vege/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def recipes(request):
+    if request.method == 'POST':
+        data=request.POST
+        recipe_name = data.get('name')
+        recipe_description=data.get('description')
+        recipe_image=request.FILES['image']
+
+        Recipe.objects.create(
+            name=recipe_name,
+            description=recipe_description,
+            image=recipe_image
+        )
+    
     return render(request, 'recipe.html')
